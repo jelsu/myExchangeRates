db48a9d2d512a11ccaada1a48964c47be03f6710
jelsu <jelus4@gmail.com>
2016-10-06 15:58:09 +0300

    add AsyncTask and Spinner

--- app/src/main/java/com/teaching/jelus/myexchangerates/MainActivity.java
+++ app/src/main/java/com/teaching/jelus/myexchangerates/MainActivity.java
@@ -3,7 +3,10 @@ package com.teaching.jelus.myexchangerates;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import org.json.JSONException;
@@ -14,12 +17,16 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class MainActivity extends AppCompatActivity {
     private TextView infoTextView;
     private TextView dateTextView;
-    private String urlString = "http://api.fixer.io/latest?base=USD";
-    private final String LOG_TAG = "my_log";
+    private Spinner currencySpinner;
+    private JSONObject dataJsonObj;
+    ArrayList<String> list = new ArrayList<>();
+    private String urlString = "http://api.fixer.io/latest?base=RUB";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,10 +34,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         infoTextView = (TextView) findViewById(R.id.infoTextView);
         dateTextView = (TextView) findViewById(R.id.dateTextView);
+        currencySpinner = (Spinner) findViewById(R.id.currencySpinner);
         new ParseTask().execute();
+        currencySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                getChangedRate();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
     }
 
-    class ParseTask extends AsyncTask<Void, Void, String> {
+    class ParseTask extends AsyncTask<Void, JSONObject, String> {
         HttpURLConnection urlConnection = null;
         BufferedReader reader = null;
         String resultJson = "";
@@ -59,20 +77,68 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(String strJson) {
             super.onPostExecute(strJson);
-            Log.d(LOG_TAG, strJson);
-            JSONObject dataJsonObj = null;
-            String eurRates = "нет данных";
-            String date = "нет данных";
             try {
                 dataJsonObj = new JSONObject(strJson);
-                date = dataJsonObj.getString("date");
-                dateTextView.setText("Текущая дата: " + date);
                 JSONObject rates = dataJsonObj.getJSONObject("rates");
-                eurRates = rates.getString("RUB");
-                infoTextView.setText("Курс доллара: " + eurRates + " руб.");
+                fillCurrencyList(rates);
+                getChangedRate();
             } catch (JSONException e) {
                 e.printStackTrace();
             }
+        }
+    }
+
+    private void fillCurrencyList(JSONObject rates){
+        String selectedItem;
+        if (currencySpinner.getSelectedItem() != null){
+            selectedItem = currencySpinner.getSelectedItem().toString();
+        } else{
+            selectedItem = "USD";
+        }
+        list.clear();
+        try {
+            for(int i = 0; i < rates.length(); i++){
+                list.add(rates.names().getString(i));
+            }
+        } catch (JSONException e){
+            e.printStackTrace();
+        }
+        ArrayAdapter<String> currencyAdapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_spinner_item,
+                list);
+        currencyAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        currencySpinner.setAdapter(currencyAdapter);
+        currencySpinner.setSelection(findSpinnerIndexByItem(selectedItem));
+    }
+
+    private int findSpinnerIndexByItem(String item){
+        int j = 0;
+        for (Iterator<String> i = list.iterator(); i.hasNext();){
+            String name = i.next();
+            if (name.equals(item)){
+                return j;
+            }
+            j++;
+        }
+        return j;
+    }
+
+    private void getChangedRate(){
+        try {
+            String date = dataJsonObj.getString("date");
+            dateTextView.setText(getResources().getString(R.string.date_text_view_string)
+                    + " "
+                    + date);
+            JSONObject rates = dataJsonObj.getJSONObject("rates");
+            double viewCheckedRate = rates.getDouble(currencySpinner.getSelectedItem().toString());
+            String rubleExchangeRate = String.format("%.2f" , 1 / viewCheckedRate);
+            infoTextView.setText(getResources().getString(R.string.rate_text_view_string)
+                    + " "
+                    + rubleExchangeRate
+                    + " руб.");
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
     }
 }
